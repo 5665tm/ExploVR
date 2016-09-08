48deec3fef0643b414eeda0128ebf2c9d3500015
5665tm <5665tm@gmail.com>
2016-09-08 15:48:57 +0300

    KEY 05 - Управление игроком с клавиатуры

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] private float _rotateFromKeyboard;
+	[SerializeField] private float _tiltFromKeyboard;
+	[SerializeField] private Transform _head;
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(Vector3.down * _rotateFromKeyboard * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(Vector3.down * ( -_rotateFromKeyboard)  * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			_head.Rotate(Vector3.left * _tiltFromKeyboard * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			_head.Rotate(Vector3.left * ( -_tiltFromKeyboard) * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Fire();
+		}
+	}
+
+	private void Fire()
+	{
+	}
+}
